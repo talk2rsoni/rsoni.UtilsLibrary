82bc99551a31712df41c81d9fef44e1681da8d98
Rajesh Soni <talk2rsoni@outlook.com>
2019-02-06 07:36:45 -0500

    Update for Nuget package

--- UtilsLibrarySolution/rsoni.UtilsLibrary/Properties/AssemblyInfo.cs
+++ UtilsLibrarySolution/rsoni.UtilsLibrary/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("rsoni.UtilsLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility methods for any console app")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rajesh's Personal")]
 [assembly: AssemblyProduct("rsoni.UtilsLibrary")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
